329fef3abfeacb785a448f51625d1846f8baa5fd
midudev <miduga@gmail.com>
2021-01-20 09:30:23 +0100

    Add Node script to fetch previous data

--- scripts/fetch-old-ods-data.js
+++ scripts/fetch-old-ods-data.js
@@ -1,5 +1,30 @@
-// Oversv: el 1º día creo que fue el 27 de diciembre
+const fs = require('fs-extra')
+const download = require('download')
+const transformOdsToJson = require('./transform-ods-to-json')
 
-const actualYear = '2021'
-const actualMonth = '01'
-const actualDay = '18'
+const createUrl = day => `https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_Comunicacion_202101${day}.ods`
+
+const downloadFile = (url, filename) => {
+  return download(url, 'data', { filename })
+    .then(async () => {
+      console.log(`${url} downloaded`)
+      const json = await transformOdsToJson(filename)
+      const jsonFileName = filename.replace('.ods', '.json')
+
+      await fs.writeJson(`./data/${jsonFileName}`, json)
+    })
+    .catch(() => {
+      console.error(`${url} can't be downloaded. Error:`)
+    })
+}
+;(async () => {
+  const days = [...Array(20).keys()].map(day => {
+    const dayToUse = `${day + 1}`.padStart(2, '0')
+    return { day: dayToUse, url: createUrl(dayToUse) }
+  })
+
+  days.reduce(
+    (promise, { day, url }) =>
+      promise.then(() => downloadFile(url, `202101${day}.ods`))
+    , Promise.resolve())
+})()
