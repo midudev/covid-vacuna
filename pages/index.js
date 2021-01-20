0d7093ebd0f00cfd80bc593657ff3e5ca757e97e
midudev <miduga@gmail.com>
2021-01-20 20:15:29 +0100

    Add content

--- pages/index.js
+++ pages/index.js
@@ -25,14 +25,14 @@ export default function Home ({ data, info }) {
           <meta property='og:type' content='website' />
 
           <meta property='og:title' content='Estado y progreso vacunación COVID-19 España 2021' />
-          <meta property='og:image' content='/og.png' />
+          <meta property='og:image' content='https://covid-vacuna.vercel.app/og.png' />
           <meta property='og:description' content='Consulta el estado y progreso de la vacunación del COVID-19 de forma diaria según datos del gobierno' />
           <meta property='og:site_name' content='Estado vacunación en España' />
 
           <meta name='twitter:card' content='summary_large_image' />
           <meta name='twitter:creator' content='midudev' />
           <meta name='twitter:description' content='Consulta el estado y progreso de la vacunación del COVID-19 de forma diaria según datos del gobierno' />
-          <meta name='twitter:image' content='/og.png' />
+          <meta name='twitter:image' content='https://covid-vacuna.vercel.app/og.png' />
           <meta name='twitter:site' content='midudev' />
           <meta name='twitter:title' content='Estado y progreso vacunación COVID-19 España 2021' />
           <meta property='twitter:domain' content='covid-vacuna.vercel.app' />
