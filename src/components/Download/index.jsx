34877ea66810fe1a52b068bcbc036062af825b76
Omar Gallo <omar.gallo@devolute.org>
2021-01-22 18:15:25 +0100

    fxied something

--- src/components/Download/index.jsx
+++ src/components/Download/index.jsx
@@ -1,15 +1,16 @@
+
 import Image from 'next/image'
 
 export default function Download ({ styles }) {
   return (
-    <a className={styles.download} download href='/data/latest.json'>
+    <a className={styles.download} download href="/data/latest.json">
       <Image
         width={32}
         height={32}
-        src='/download.png'
-        alt='Descargar datos'
+        src="/download.png"
+        alt="Descargar datos"
       />
       Descargar últimos datos en formato JSON
     </a>
-  )
+  );
 }
