13906683eef1152fa2f400400eb9da49f79065ae
Cristian Enguídanos Nebot <cenguidanos@protonmail.com>
2021-01-23 19:10:12 +0100

    Cambiar formato del valor yaxis a exponencial
    * Se añade un formateo para representar exponencialmente los valores
    de 6 cifras en las gráficas y así ganar un poco de espacio en el margen.

--- components/ProgressChart/ticks/index.jsx
+++ components/ProgressChart/ticks/index.jsx
@@ -18,6 +18,9 @@ export function CustomXTick ({ x, y, payload }) {
 }
 
 export function CustomYTick ({ x, y, payload }) {
+  const exponentFormatValue = payload.value.toExponential(0)
+  const omitZero = payload.value === 0 ? '' : exponentFormatValue
+
   return (
     <g transform={`translate(${x},${y})`}>
       <text
@@ -29,7 +32,7 @@ export function CustomYTick ({ x, y, payload }) {
         textAnchor='end'
         fill='var(--text-subtitle-color)'
       >
-        {new Intl.NumberFormat('es-ES').format(payload.value)}
+        {omitZero}
       </text>
     </g>
   )
