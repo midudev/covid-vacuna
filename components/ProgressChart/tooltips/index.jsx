9e73a1039cb2572c2f419330c9304ea5e0b7ac8c
Alfredo de la Calle <alfredo.delacalle.contractor@bbva.com>
2021-01-27 22:16:37 +0100

    chore: add values of companies & totals

--- components/ProgressChart/tooltips/index.jsx
+++ components/ProgressChart/tooltips/index.jsx
@@ -1,9 +1,7 @@
 import styles from '../styles/ProgressChart.module.css'
 
-function formatNumberToLocale (payload, locale) {
-  if (!payload) return
-
-  const { value } = payload.pop()
+function formatNumberToLocale (value, locale = 'es-ES') {
+  if (value === undefined) return
 
   const num = new Intl.NumberFormat(locale)
   return num.format(value)
@@ -14,31 +12,52 @@ function Bold ({ text }) {
 }
 
 export function DosisAdministradasTooltip ({ active, payload, label }) {
-  if (!active) return null
+  if (!active || !payload || (payload && payload.length === 0)) return null
 
-  const value = formatNumberToLocale(payload, 'es-ES')
+  const { value } = payload[0]
+
+  const _value = formatNumberToLocale(value)
 
   return (
     <div className={styles.chartTooltip}>
       <p>
         A día <Bold text={label} /> se han administrado{' '}
-        <Bold text={value} /> dosis.
+        <Bold text={_value} /> dosis.
       </p>
     </div>
   )
 }
 
 export function DosisEntregadasTooltip ({ active, payload, label }) {
-  if (!active) return null
+  if (!active || !payload || (payload && payload.length === 0)) return null
 
-  const value = formatNumberToLocale(payload, 'es-ES')
+  const { payload: _payload } = payload[0]
+
+  const esEmpresa = empresa => !['name'].includes(empresa)
+
+  const empresas = Object.keys(_payload)
+    .filter(esEmpresa)
+
+  // Se realiza el sumatorio eliminando el atributo "name"
+  const total = empresas
+    .reduce((total, empresa) => {
+      total = esEmpresa(empresa)
+        ? total + _payload[empresa]
+        : total
+
+      return total
+    }, 0)
 
   return (
     <div className={styles.chartTooltip}>
-      <p>
-        A día <Bold text={label} /> se han entregado{' '}
-        <Bold text={value} /> dosis.
+      <p>A día <Bold text={label} /> se han entregado{' '} un total de
+        {' '}<Bold text={formatNumberToLocale(total)} /> dosis.
       </p>
+      <ul>
+        {empresas.map((empresa, index) => {
+          return <li key={index}>{empresa}: {formatNumberToLocale(_payload[empresa])}</li>
+        })}
+      </ul>
     </div>
   )
 }
