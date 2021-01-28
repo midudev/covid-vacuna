ea528e4b4a052ca20e491cd72dcee3fc62c78d4a
midudev <miduga@gmail.com>
2021-01-28 23:29:31 +0100

    Add blank lines for useHasMounted

--- hooks/useHasMounted.js
+++ hooks/useHasMounted.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from 'react'
 
 export default function useHasMounted () {
   const [hasMounted, setHasMounted] = useState(false)
+
   useEffect(() => {
     setHasMounted(true)
   }, [])
+
   return hasMounted
 }
