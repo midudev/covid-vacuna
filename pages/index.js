b975a3f7b903bb5b186f23025dd2ddcb3ba31746
midudev <miduga@gmail.com>
2021-02-11 01:31:40 +0100

    Fix typo translation

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function Home ({ contributors, data, info, reports, chartDatasets
       <div id='container' className={styles.container}>
         <main className={styles.main}>
           <h1 className={styles.title}>
-            {translate.home.tituloPricipal} {filter === 'Totales' ? 'España' : filter}
+            {translate.home.tituloPrincipal} {filter === 'Totales' ? 'España' : filter}
           </h1>
           <small className={styles.description}>
             {translate.home.datosActualizados} <TimeAgo timestamp={info.lastModified} />.
