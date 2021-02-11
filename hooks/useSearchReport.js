dc12caa61f36b8b479612fdd853878af60fa5a4b
midudev <miduga@gmail.com>
2021-02-11 01:03:32 +0100

    Avoid breaking first render

--- hooks/useSearchReport.js
+++ hooks/useSearchReport.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react'
 
 export default function useSearch ({ valueSearch }) {
-  const [resultSearch, setresultSearch] = useState()
+  const [resultSearch, setResultSearch] = useState()
 
   useEffect(() => {
     if (valueSearch !== '') {
       window.fetch(`../data/${valueSearch}.json`)
-        .then((res) => res.json())
-        .then(setresultSearch)
+        .then(res => res.json())
+        .then(setResultSearch)
     }
   }, [valueSearch])
 
