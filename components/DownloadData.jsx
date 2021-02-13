dbe6d7e3909f47471c5acd9dcca5ea4a7e7e09d8
sanchis <sanchis1992@gmail.com>
2021-02-13 22:06:01 +0100

    fix: download link data only when valueSearch is valid

--- components/DownloadData.jsx
+++ components/DownloadData.jsx
@@ -8,18 +8,23 @@ export default function DownloadData ({ valueSearch }) {
   const translate = useTranslate()
   const { locale } = useLocale()
   const dataFilePath = `../data/${valueSearch}.json`
+  const canShowLink = ((valueSearch !== null || valueSearch !== undefined) && valueSearch !== '')
 
   const date = valueSearch ? `(${formatDate({ locale, value: valueSearch })})` : ''
 
   return (
-    <a className={styles.download} download href={dataFilePath}>
-      <Image
-        width={32}
-        height={32}
-        src='/download.png'
-        alt={translate.home.alt.descargarDatos}
-      />
-      {translate.home.descargarDatosJSON}{date}
-    </a>
+    canShowLink
+      ? (
+        <a className={styles.download} download href={dataFilePath}>
+          <Image
+            width={32}
+            height={32}
+            src='/download.png'
+            alt={translate.home.alt.descargarDatos}
+          />
+          {translate.home.descargarDatosJSON}{date}
+        </a>
+        )
+      : null
   )
 }
