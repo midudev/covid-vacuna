85ad8ab74de1b1c62f1c949c727eeb1112ccc1de
midudev <miduga@gmail.com>
2021-03-29 20:47:31 +0200

    feat(Root): Upgrade to new next.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const withPWA = require('next-pwa')
 const withImages = require('next-images')
 const withPlugins = require('next-compose-plugins')
 
+const nextConfig = {
+  future: {
+    webpack5: true,
+    strictPostcssConfiguration: true
+  },
+  i18n: {
+    localeDetection: false,
+    locales: ['es', 'ca', 'gl', 'eu'],
+    defaultLocale: 'es'
+  }
+}
+
 module.exports = withPlugins([
   [
     withImages,
@@ -21,10 +33,4 @@ module.exports = withPlugins([
       }
     }
   ]
-], {
-  i18n: {
-    localeDetection: false,
-    locales: ['es', 'ca', 'gl', 'eu'],
-    defaultLocale: 'es'
-  }
-})
+], nextConfig)
