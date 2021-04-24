06627d46c301bd6483a363292cf826472320d221
midudev <miduga@gmail.com>
2021-04-24 22:55:25 +0200

    Update old data to get etarios

--- scripts/fetch-old-ods-data.js
+++ scripts/fetch-old-ods-data.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const download = require('download')
 const transformOdsToJson = require('./transform-ods-to-json')
 
-const createUrl = day => `https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_Comunicacion_202102${day}.ods`
+const createUrl = day => `https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_Comunicacion_202104${day}.ods`
 
 const downloadFile = (url, filename) => {
   return download(url, 'public/data', { filename })
@@ -18,13 +18,13 @@ const downloadFile = (url, filename) => {
     })
 }
 ;(async () => {
-  const days = [...Array(22).keys()].map(day => {
+  const days = [...Array(23).keys()].map(day => {
     const dayToUse = `${day + 1}`.padStart(2, '0')
     return { day: dayToUse, url: createUrl(dayToUse) }
   })
 
   days.reduce(
     (promise, { day, url }) =>
-      promise.then(() => downloadFile(url, `202101${day}.ods`))
+      promise.then(() => downloadFile(url, `202104${day}.ods`))
     , Promise.resolve())
 })()
