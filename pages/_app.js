fcaedfc075f5b8f70d568a318da8282238790d36
Mateo Santos <mateo@vitalinnova.com>
2021-06-29 23:31:20 +0200

    Make meta title and meta description translatable

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { DefaultSeo } from 'next-seo'
 
+import { useTranslate } from 'hooks/useTranslate'
 import SEO from 'config/seo'
 import useFathom from 'hooks/useFathom'
 
@@ -7,7 +8,10 @@ import 'styles/globals.css'
 
 function App ({ Component, pageProps }) {
   useFathom({ ID: 'MYEXKUNW', domains: ['covid-vacuna.app'] })
+  const translate = useTranslate()
 
+  SEO.title = translate.seo.title
+  SEO.description = translate.seo.description
   return (
     <>
       <DefaultSeo {...SEO} />
