3ecae96330c62eddb7cbafa4d24632ff3b63275d
midudev <miduga@gmail.com>
2021-10-28 15:22:34 +0200

    Add polyfill for IntlRelativeTimeFormat

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import { DefaultSeo } from 'next-seo'
 import SEO from 'config/seo'
 import useFathom from 'hooks/useFathom'
 
+import '@formatjs/intl-relativetimeformat/polyfill'
+import '@formatjs/intl-relativetimeformat/locale-data/es'
+
 import 'styles/globals.css'
 
 function App ({ Component, pageProps }) {
